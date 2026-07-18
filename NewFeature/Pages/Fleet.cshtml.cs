c34c0e6ad8570f22fd55b95e19a735460e5ab6b6
Ahmed-Abdelaziz <ahmed.abdelaziz@zone.net.sa>
2026-07-18 14:33:50 +0300

    enhance design

---
+++ NewFeature/Pages/Fleet.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NewFeature.Pages
+{
+    public class FleetModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
